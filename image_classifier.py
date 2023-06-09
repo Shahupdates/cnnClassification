7bb78ba0bb5eacd4f75e2d77f3a25764f34ac413
Shahupdates <120000782+Shahupdates@users.noreply.github.com>
2023-06-09 17:40:04 -0400

    Update image_classifier.py
     update your image_classifier.py file to use the methods in preprocessing.py, evaluation.py, and model_utils.py.

--- image_classifier.py
+++ image_classifier.py
@@ -1,10 +1,8 @@
 # image_classifier.py
 
-import tensorflow as tf
-from tensorflow.keras.applications import ResNet50
-from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
-from tensorflow.keras.models import Model
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from preprocessing import get_image_data_generator
+from evaluation import evaluate_model, plot_confusion_matrix, plot_learning_curve
+from model_utils import create_model, save_model, load_model
 
 class ImageClassifier:
     def __init__(self):
@@ -14,18 +12,8 @@ class ImageClassifier:
         input_shape = (224, 224, 3)
 
         # Create data generators with augmentation
-        train_datagen = ImageDataGenerator(
-            rescale=1./255,
-            rotation_range=20,
-            width_shift_range=0.2,
-            height_shift_range=0.2,
-            shear_range=0.2,
-            zoom_range=0.2,
-            horizontal_flip=True,
-            vertical_flip=True
-        )
-
-        test_datagen = ImageDataGenerator(rescale=1./255)
+        train_datagen = get_image_data_generator()
+        test_datagen = get_image_data_generator(rotation_range=0, width_shift_range=0, height_shift_range=0, shear_range=0, zoom_range=0, horizontal_flip=False, vertical_flip=False)
 
         train_generator = train_datagen.flow_from_directory(
             train_dir,
@@ -41,21 +29,8 @@ class ImageClassifier:
             class_mode='categorical'
         )
 
-        # Load pre-trained ResNet50 model
-        base_model = ResNet50(weights='imagenet', include_top=False, input_shape=input_shape)
-
-        # Add custom classifier on top
-        x = base_model.output
-        x = GlobalAveragePooling2D()(x)
-        x = Dense(256, activation='relu')(x)
-        predictions = Dense(10, activation='softmax')(x)
-
-        # Create the complete model
-        self.model = Model(inputs=base_model.input, outputs=predictions)
-
-        # Freeze pre-trained layers
-        for layer in base_model.layers:
-            layer.trainable = False
+        # Create the model
+        self.model = create_model(input_shape)
 
         # Compile the model
         self.model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
@@ -69,9 +44,22 @@ class ImageClassifier:
             validation_steps=test_generator.n // batch_size
         )
 
+        # Plot learning curve
+        plot_learning_curve(history)
+
+    def evaluate(self, test_dir, batch_size=32):
+        input_shape = (224, 224, 3)
+        test_datagen = get_image_data_generator(rotation_range=0, width_shift_range=0, height_shift_range=0, shear_range=0, zoom_range=0, horizontal_flip=False, vertical_flip=False)
+        test_generator = test_datagen.flow_from_directory(
+            test_dir,
+            target_size=input_shape[:2],
+            batch_size=batch_size,
+            class_mode='categorical'
+        )
+        evaluate_model(self.model, test_generator)
+
     def save_model(self, model_path):
-        if self.model:
-            self.model.save(model_path)
+        save_model(self.model, model_path)
 
     def load_model(self, model_path):
-        self.model = tf.keras.models.load_model(model_path)
+        self.model = load_model(model_path)
