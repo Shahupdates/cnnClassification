accdc40ceebe19c3ff6d4db365b2b966147f476f
Shahupdates <120000782+Shahupdates@users.noreply.github.com>
2023-06-09 17:37:32 -0400

    Create preprocessing.py
    A module for all the data preprocessing needs, including resizing images, normalization, data augmentation, and splitting data into training and testing sets.

---
+++ preprocessing.py
@@ -0,0 +1,13 @@
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+def get_image_data_generator(rescale=1./255, rotation_range=20, width_shift_range=0.2, height_shift_range=0.2, shear_range=0.2, zoom_range=0.2, horizontal_flip=True, vertical_flip=True):
+    return ImageDataGenerator(
+        rescale=rescale,
+        rotation_range=rotation_range,
+        width_shift_range=width_shift_range,
+        height_shift_range=height_shift_range,
+        shear_range=shear_range,
+        zoom_range=zoom_range,
+        horizontal_flip=horizontal_flip,
+        vertical_flip=vertical_flip
+    )
