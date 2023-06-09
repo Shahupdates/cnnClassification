df32c0f6543cec398d07bc79a6a97e7a1718884a
Shahupdates <120000782+Shahupdates@users.noreply.github.com>
2023-06-09 17:34:07 -0400

    Update image_classifier.py

--- image_classifier.py
+++ image_classifier.py
@@ -1,73 +1,77 @@
+# image_classifier.py
+
 import tensorflow as tf
 from tensorflow.keras.applications import ResNet50
 from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
 from tensorflow.keras.models import Model
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-# Set hyperparameters
-batch_size = 32
-epochs = 10
-input_shape = (224, 224, 3)
+class ImageClassifier:
+    def __init__(self):
+        self.model = None
 
-# Create data generators with augmentation
-train_datagen = ImageDataGenerator(
-    rescale=1./255,
-    rotation_range=20,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True,
-    vertical_flip=True
-)
+    def train(self, train_dir, test_dir, batch_size=32, epochs=10):
+        input_shape = (224, 224, 3)
 
-test_datagen = ImageDataGenerator(rescale=1./255)
+        # Create data generators with augmentation
+        train_datagen = ImageDataGenerator(
+            rescale=1./255,
+            rotation_range=20,
+            width_shift_range=0.2,
+            height_shift_range=0.2,
+            shear_range=0.2,
+            zoom_range=0.2,
+            horizontal_flip=True,
+            vertical_flip=True
+        )
 
-train_generator = train_datagen.flow_from_directory(
-    'train',
-    target_size=input_shape[:2],
-    batch_size=batch_size,
-    class_mode='categorical'
-)
+        test_datagen = ImageDataGenerator(rescale=1./255)
 
-test_generator = test_datagen.flow_from_directory(
-    'test',
-    target_size=input_shape[:2],
-    batch_size=batch_size,
-    class_mode='categorical'
-)
+        train_generator = train_datagen.flow_from_directory(
+            train_dir,
+            target_size=input_shape[:2],
+            batch_size=batch_size,
+            class_mode='categorical'
+        )
 
-# Load pre-trained ResNet50 model
-base_model = ResNet50(weights='imagenet', include_top=False, input_shape=input_shape)
+        test_generator = test_datagen.flow_from_directory(
+            test_dir,
+            target_size=input_shape[:2],
+            batch_size=batch_size,
+            class_mode='categorical'
+        )
 
-# Add custom classifier on top
-x = base_model.output
-x = GlobalAveragePooling2D()(x)
-x = Dense(256, activation='relu')(x)
-predictions = Dense(10, activation='softmax')(x)
+        # Load pre-trained ResNet50 model
+        base_model = ResNet50(weights='imagenet', include_top=False, input_shape=input_shape)
 
-# Create the complete model
-model = Model(inputs=base_model.input, outputs=predictions)
+        # Add custom classifier on top
+        x = base_model.output
+        x = GlobalAveragePooling2D()(x)
+        x = Dense(256, activation='relu')(x)
+        predictions = Dense(10, activation='softmax')(x)
 
-# Freeze pre-trained layers
-for layer in base_model.layers:
-    layer.trainable = False
+        # Create the complete model
+        self.model = Model(inputs=base_model.input, outputs=predictions)
 
-# Compile the model
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+        # Freeze pre-trained layers
+        for layer in base_model.layers:
+            layer.trainable = False
 
-# Train the model with fine-tuning
-history = model.fit(
-    train_generator,
-    steps_per_epoch=train_generator.n // batch_size,
-    epochs=epochs,
-    validation_data=test_generator,
-    validation_steps=test_generator.n // batch_size
-)
+        # Compile the model
+        self.model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-# Evaluate the model
-score = model.evaluate(test_generator, verbose=0)
+        # Train the model with fine-tuning
+        history = self.model.fit(
+            train_generator,
+            steps_per_epoch=train_generator.n // batch_size,
+            epochs=epochs,
+            validation_data=test_generator,
+            validation_steps=test_generator.n // batch_size
+        )
 
-# Print the test loss and accuracy
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+    def save_model(self, model_path):
+        if self.model:
+            self.model.save(model_path)
+
+    def load_model(self, model_path):
+        self.model = tf.keras.models.load_model(model_path)
