b8d286e7cdd4766fa6d4e7171d31cfba2e6a996f
Shahupdates <120000782+Shahupdates@users.noreply.github.com>
2023-06-11 08:36:34 -0400

    Update model_utils.py
    Potential improvements:
    Model fine-tuning: The pre-trained ResNet50 model is frozen in your current implementation. Depending on the dataset and task, you might want to fine-tune the weights of some of the top layers.
    
    Regularization: You can add regularization such as dropout or batch normalization to prevent overfitting.
    
    Configurable classes: To make the function more flexible, consider adding arguments for the number of dense layers, the number of nodes in each dense layer, and the number of classes.

--- model_utils.py
+++ model_utils.py
@@ -1,17 +1,23 @@
 import tensorflow as tf
 from tensorflow.keras.applications import ResNet50
-from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.keras.layers import Dense, Dropout, GlobalAveragePooling2D, BatchNormalization
 from tensorflow.keras.models import Model
 
-def create_model(input_shape=(224, 224, 3)):
+def create_model(input_shape=(224, 224, 3), num_classes=10, num_nodes=256, dropout_rate=0.5, unfreeze_layers=5):
     # Load pre-trained ResNet50 model
     base_model = ResNet50(weights='imagenet', include_top=False, input_shape=input_shape)
+
+    # Unfreeze the top layers of ResNet
+    for layer in base_model.layers[:-unfreeze_layers]:
+        layer.trainable = False
 
     # Add custom classifier on top
     x = base_model.output
     x = GlobalAveragePooling2D()(x)
-    x = Dense(256, activation='relu')(x)
-    predictions = Dense(10, activation='softmax')(x)
+    x = Dense(num_nodes, activation='relu')(x)
+    x = BatchNormalization()(x)
+    x = Dropout(dropout_rate)(x)
+    predictions = Dense(num_classes, activation='softmax')(x)
 
     # Create the complete model
     model = Model(inputs=base_model.input, outputs=predictions)
